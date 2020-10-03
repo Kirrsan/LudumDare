8f644328067a80a599e68b28a98ca4f86e5bd15b
Grégoire <gpierillas@gmail.com>
2020-10-03 14:34:34 +0200

    [Feat] PlayerController and capacity interface
    Jump and hide classes are just placeholder

---
+++ Ludum Dare 47/Assets/Scripts/PLACEHOLDER_Hide.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hide : ICapacity
+{
+    public void UseCapacity()
+    {
+        Debug.Log("hide");
+    }
+}
---
+++ Ludum Dare 47/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private ICapacity _capacity;
+    private Jump _jumpCapacity;
+    private Hide _hideCapacity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _jumpCapacity = new Jump();
+        _hideCapacity = new Hide();
+        _capacity = _jumpCapacity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInput();
+    }
+    
+    void CheckInput()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            _capacity.UseCapacity();
+        }
+
+        //switch Test
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (_capacity == _jumpCapacity)
+                _capacity = _hideCapacity;
+            else if (_capacity == _hideCapacity)
+                _capacity = _jumpCapacity;
+        }
+    }
+
+    public void SetCapacityDoubleJump()
+    {
+        //_capacity = doubleJumpScript;
+    }
+    
+    public void SetCapacityWall()
+    {
+        //_capacity = WallRunScript;
+    }
+}
