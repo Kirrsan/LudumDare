88b14d354a3595caad952b87dd8604c6a72f9ddc
alexandrejanin <alexandrejanin92@gmail.com>
2020-10-03 15:00:17 +0200

    LevelManager

---
+++ Ludum Dare 47/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+    [SerializeField] private GameObject[] worlds;
+
+    private int currentWorld;
+
+    public int CurrentWorld => currentWorld;
+
+    private void Start() {
+        UpdateWorlds();
+    }
+
+    private void UpdateWorlds() {
+        for (var i = 0; i < worlds.Length; i++) {
+            worlds[i].SetActive(i == currentWorld);
+        }
+    }
+
+    public void ActivateNextWorld() {
+        currentWorld++;
+        currentWorld %= worlds.Length;
+        UpdateWorlds();
+    }
+}
