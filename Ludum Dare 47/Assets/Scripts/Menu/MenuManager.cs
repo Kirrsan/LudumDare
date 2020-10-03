6c124055f84ef4cf16db987d50a2bdd7566daf51
Matilde <matilde.rodrigues@edu.devinci.fr>
2020-10-03 14:24:55 +0200

    [FEAT] Menu placeholder done

---
+++ Ludum Dare 47/Assets/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+
+    [SerializeField] private GameObject basePanel;
+    [SerializeField] private GameObject optionsPanel;
+    [SerializeField] private int sceneToLoad = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GoToBasePanel();
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+    public void GoToBasePanel()
+    {
+        basePanel.SetActive(true);
+        optionsPanel.SetActive(false);
+    }
+
+    public void Options()
+    {
+        basePanel.SetActive(false);
+        optionsPanel.SetActive(true);
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
+    }
+}
