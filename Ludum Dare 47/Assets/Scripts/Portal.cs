6a91c754b08e6ea028e3cfea3674d88aeeadd4ae
Str00ke <charlelie.steam@gmail.com>
2020-10-03 15:54:44 +0200

    Adding LevelManager to Manager Prefab, Add Portal script: Player detection, change world.

---
+++ Ludum Dare 47/Assets/Scripts/Portal.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+
+    BoxCollider boxCollider;
+    public LevelManager lvlManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boxCollider = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    /*private void on
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            Debug.Log("yes");
+            lvlManager.ActivateNextWorld();
+        }
+    }*/
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            
+            lvlManager.ActivateNextWorld();
+        }
+    }
+}
