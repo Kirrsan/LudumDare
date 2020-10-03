c268cc9d8071f69fbc89a3fedf7f8cc66cb81320
Matilde <matilde.rodrigues@edu.devinci.fr>
2020-10-03 14:51:56 +0200

    [FEAT] GameManager + pause menu set up

---
+++ Ludum Dare 47/Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum State { PAUSE, INGAME, WIN, LOOSE }
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance;
+
+    public State state;
+    public System.Action onStateChange;
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ChangeState(State.INGAME);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape) && state != State.PAUSE)
+        {
+            ChangeState(State.PAUSE);
+        }
+        else if (Input.GetKeyUp(KeyCode.Escape) && state == State.PAUSE)
+        {
+            ChangeState(State.INGAME);
+        }
+    }
+
+    public void ChangeState(State newState)
+    {
+        state = newState;
+        if (onStateChange != null) onStateChange.Invoke();
+    }
+}
---
+++ Ludum Dare 47/Assets/Scripts/Manager/InterfaceManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InterfaceManager : MonoBehaviour
+{
+
+    [SerializeField] private GameObject gamePanel;
+    [SerializeField] private GameObject pausePanel;
+    [SerializeField] private GameObject winPanel;
+    [SerializeField] private GameObject loosePanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameManager.instance.onStateChange += () => {
+            if (GameManager.instance.state == State.PAUSE)
+            {
+                GoInpause();
+            }
+            else if (GameManager.instance.state == State.INGAME)
+            {
+                GoToGame();
+            }
+        };
+        GoToGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void GoInpause()
+    {
+        gamePanel.SetActive(false);
+        pausePanel.SetActive(true);
+    }
+
+    private void GoToGame()
+    {
+        pausePanel.SetActive(false);
+        winPanel.SetActive(false);
+        loosePanel.SetActive(false);
+        gamePanel.SetActive(true);
+    }
+}
