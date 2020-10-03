78e13c66eea23315d117e1c6d74e2fb01c9c2a48
Grégoire <gpierillas@gmail.com>
2020-10-03 15:20:24 +0200

    [Feat] Ground Detection on Tag "Sol"
    Trigger Enter puts bool true
    Trigger exit puts it false
    Need a trigger and a collider on player

--- Ludum Dare 47/Assets/Scripts/PLACEHOLDER_Jump.cs
+++ Ludum Dare 47/Assets/Scripts/PLACEHOLDER_Jump.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Jump : ICapacity
 {
-    public void UseCapacity(Rigidbody rb)
+    public void UseCapacity(Rigidbody rb,bool onGround)
     {
         Debug.Log("jump");
     }
