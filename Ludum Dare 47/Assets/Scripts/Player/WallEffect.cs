4d97747e628fc3fe9ed97b503db4e58f3555e1fd
Matilde <matilde.rodrigues@edu.devinci.fr>
2020-10-04 09:09:23 +0200

    [FEAT] Sparks when on wall grind

---
+++ Ludum Dare 47/Assets/Scripts/Player/WallEffect.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallEffect : MonoBehaviour
+{
+
+    [SerializeField] private GameObject _particles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Wall"))
+        {
+            _particles.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Wall"))
+        {
+            _particles.SetActive(false);
+        }
+    }
+
+}
