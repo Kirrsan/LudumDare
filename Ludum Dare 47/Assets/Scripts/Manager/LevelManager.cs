efdcaebdc7a298e6eab5f74f5224e1551a4ddb6e
Ulysse <ulysse.raillon@hotmail.fr>
2020-10-04 13:51:55 +0200

    Skybox :Clean packages

--- Ludum Dare 47/Assets/Scripts/Manager/LevelManager.cs
+++ Ludum Dare 47/Assets/Scripts/Manager/LevelManager.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour {
 
     public static LevelManager instance;
 
-    [SerializeField] private GameObject[] worlds;
+    [SerializeField] private Worlds[] worlds;
 
     private int currentWorld;
 
@@ -26,8 +28,9 @@ public class LevelManager : MonoBehaviour {
 
     private void UpdateWorlds() {
         for (var i = 0; i < worlds.Length; i++) {
-            worlds[i].SetActive(i == currentWorld);
+            worlds[i].gameObjects.SetActive(i == currentWorld);
         }
+        SwitchSkybox();
     }
 
     public void ActivateNextWorld() {
@@ -35,4 +38,18 @@ public class LevelManager : MonoBehaviour {
         currentWorld %= worlds.Length;
         UpdateWorlds();
     }
+
+    private void SwitchSkybox()
+    {
+        RenderSettings.skybox = worlds[currentWorld].skybox;
+        RenderSettings.fogColor = worlds[currentWorld].color;
+    }
+}
+
+[System.Serializable]
+public struct Worlds
+{
+    public Material skybox;
+    public Color color;
+    public GameObject gameObjects;
 }
