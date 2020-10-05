9010014a079b2e7c7b453462066d0c7d63b1ac6e
Matilde <matilde.rodrigues@edu.devinci.fr>
2020-10-05 21:17:39 +0200

    [FEAT] Sound on step

---
+++ Ludum Dare 47/Assets/Scripts/Player/PlaySoundOnStep.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundOnStep : MonoBehaviour
+{
+    public void PlayStep()
+    {
+        print("miuaou");
+        AudioManager.instance.Play("Step");
+    }
+}
